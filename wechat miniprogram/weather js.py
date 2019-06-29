0a83d8de4acc7fc18e2d343efa6dd3086c4151d8
Hylan129 <jyzyg129@163.com>
2019-06-29 21:07:22 +0800

    微信小程序研究
    微信小程序研究

---
+++ wechat miniprogram/weather js.py
@@ -0,0 +1,37 @@
+{
+    "HeWeather6": [
+        {
+            "basic": {
+                "cid": "CN101010100", 
+                "location": "北京", 
+                "parent_city": "北京", 
+                "admin_area": "北京", 
+                "cnty": "中国", 
+                "lat": "39.90498734", 
+                "lon": "116.4052887", 
+                "tz": "+8.00"
+            }, 
+            "update": {
+                "loc": "2019-05-12 18:12", 
+                "utc": "2019-05-12 10:12"
+            }, 
+            "status": "ok", 
+            "now": {
+                "cloud": "91", 
+                "cond_code": "503", 
+                "cond_txt": "扬沙", 
+                "fl": "14", 
+                "hum": "60", 
+                "pcpn": "0.0", 
+                "pres": "1006", 
+                "tmp": "15", 
+                "vis": "5", 
+                "wind_deg": "70", 
+                "wind_dir": "东北风", 
+                "wind_sc": "2", 
+                "wind_spd": "6"
+            }
+        }
+    ]
+}
+
