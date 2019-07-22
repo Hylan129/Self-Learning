ade6e461c6d0cc0705fde9fc9bfff523a94985c0
Hylan129 <jyzyg129@163.com>
2019-07-22 13:52:33 +0800

    程序算法更新

--- switch_the wifi_connect/调试切网/client.py
+++ switch_the wifi_connect/调试切网/client.py
@@ -2,25 +2,90 @@
 # -*- coding:utf-8 -*-
 
 import socket
+import tkinter #导入tkinter模块
+import tkinter.messagebox
+import tkinter.filedialog
+from tkinter import *
 
-#1、创建一个socket套接字对象,同时需要指定网络通信所用的协议,在这里用的是TCP协议
-socket_client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+root  = tkinter.Tk()
+root.minsize(500,300)
+root.maxsize(500,300)
 
-#2、通过socket这个套接字向服务端发送链接请求
-#ip = socket.gethostbyname(socket.gethostname())
-socket_client.connect(("10.7.5.105",8081))
+root.title("小π网络切换工具V01")
 
-"""
-从下面开始双方将会基于这个链接进行相互通信
-"""
-while True:
-    msg = input(">>:").strip()
-    if not msg: continue
-    socket_client.send(msg.encode("utf-8"))
+#SSID_INIT = PWD_INIT = ''
+command_SSID = tkinter.StringVar()
+command_PWD = tkinter.StringVar()
+    
+button1 = tkinter.Button(root,text='刷新服务',command = lambda : send_command_update('5'))
+button1.place(x=40,y=20,width=85,height=40)
+
+button2 = tkinter.Button(root,text='切换主板网络',command = lambda : msg_input('1'))
+button2.place(x=135,y=20,width=85,height=40)
+
+button2 = tkinter.Button(root,text='切换六麦网络',command = lambda : msg_input('6'))
+button2.place(x=225,y=20,width=85,height=40)
+
+def send_command_update(msg):
+
+    socket_client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    try:
+        socket_client.connect(("10.7.5.105",8081))
+        socket_client.send(msg.encode("utf-8"))
+    except:
+        tkinter.messagebox.showerror('错误', '请连接导航盒WIFI后再试！')
 
     #server_msg = socket_client.recv(1024)
     server_msg = socket_client.recv(2048).decode("gb2312",'replace')
     print("服务端发送过来的数据是:%s"%server_msg)
+    socket_client.close()
+    tkinter.messagebox.showinfo('提示',server_msg)
 
+def msg_input(number):
+    
+    msg_in = Toplevel(root)
+    msg_in.minsize(300,200)
+    msg_in.maxsize(300,200)
+    
+    msg_in.title("请输入网络账号密码")
+    
+    def setting():
+        SSID_INIT = command_SSID.get()
+        #print("SSID_INIT:"+ SSID_INIT)
+        PWD_INIT = command_PWD.get()
+        #print("PWD_INIT:"+ PWD_INIT)
+        msg_in.destroy()
+        send_command_update_2(number,SSID_INIT.strip(),PWD_INIT.strip())
+        
+    label1 = tkinter.Label(msg_in,text="SSID")
+    label1.place(x=10,y=42,width=100)
 
-socket_client.close()
+    entry1 = tkinter.Entry(msg_in,textvariable = command_SSID)
+    entry1.place(x = 100,y=42,width=180,height=30)
+    
+    label2 = tkinter.Label(msg_in,text="PASSWORD")
+    label2.place(x=10,y=92,width=100)
+
+    entry2 = tkinter.Entry(msg_in,textvariable = command_PWD)
+    entry2.place(x = 100,y=92,width=180,height=30)
+    
+    button0 = tkinter.Button(msg_in,text="提交",command=lambda: setting())
+    button0.place(x=120,y=140)
+    
+    
+def send_command_update_2(number,ssid,pwd):
+    socket_client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    try:
+        socket_client.connect(("10.7.5.105",8081))
+    except:
+        tkinter.messagebox.showerror('错误', '请连接导航盒WIFI后再试！')
+    
+    socket_client.send((number+','+ssid+','+pwd).encode("utf-8"))
+
+    #server_msg = socket_client.recv(1024)
+    server_msg = socket_client.recv(2048).decode("gb2312",'replace')
+    print("服务端发送过来的数据是:%s"%server_msg)
+    socket_client.close()
+    tkinter.messagebox.showinfo('提示',server_msg)
+root.mainloop()
+
--- switch_the wifi_connect/调试切网/server.py
+++ switch_the wifi_connect/调试切网/server.py
@@ -4,6 +4,9 @@
 import socket
 import subprocess
 
+import computer_switch
+import sixmic_switch
+
 #创建socket套接字,并指定通信所用的协议
 socket_server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 #重新使用IP地址和端口号
@@ -15,28 +18,33 @@ socket_server.listen(5)
 while True:
     print("等待客户端的连接.........")
     conn,address = socket_server.accept()
-    print("链接已经生成")
-    print("客户端的信息是:",address)
+    print("链接已经生成！客户端的信息是:",address)
+    print("1-->切换主板网络，5-->刷新服务，6-->切换六麦网络！")
 
     """
     双方建立了连接后,就开始进行相同通信
     """
     while True:
         try:
-            client_msg = conn.recv(1024).decode("utf-8")
+            client_msg = conn.recv(1024).decode("gb2312",'replace').strip().split(',')
+            print(client_msg)
             
-            print("客户端发送的消息是:%s" %client_msg)
-            res = subprocess.Popen(client_msg,shell=True,
-                                   stdout=subprocess.PIPE,
-                                   stderr=subprocess.PIPE)
-            res_err = res.stderr.read()
-            if res_err:
-                cmd_res = res_err
+            if(client_msg[0].strip()=='1' and len(client_msg)==3):
+                feedback = computer_switch.wifi_switch(client_msg[1].strip(),client_msg[2].strip())
+                conn.send(feedback.encode("gb2312",'replace'))
+            elif (client_msg[0].strip()=='6' and len(client_msg)==3):
+                feedback = sixmic_switch.wifi_switch(client_msg[1].strip(),client_msg[2].strip()) 
+                conn.send(feedback.encode("gb2312",'replace'))
+            elif (client_msg[0].strip()=='5' and len(client_msg)==1):
+                feedback = sixmic_switch.kill_server()
+                conn.send(feedback.encode("gb2312",'replace'))
             else:
-                cmd_res = res.stdout.read()
-            conn.send(cmd_res)
+                feedback = "输入错误，请重新输入！1-->切换主板网络，5-->刷新服务，6-->切换六麦网络！"
+                conn.send(feedback.encode("gb2312",'replace'))
 
-        except Exception:
+        except Exception as e:
+            with open('err.txt','a') as code:
+                code.write("server:"+str(e)+'\n')
             break
 
     conn.close()
