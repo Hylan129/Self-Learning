c6ed1b7edb755f70283c84c2f3bc76cdcca5131a
Hylan129 <jyzyg129@163.com>
2019-12-18 15:53:08 +0800

    Update FourG.py
    增加4G板串口输入为空的处理情况

--- robot_application/software_version_check/package/FourG.py
+++ robot_application/software_version_check/package/FourG.py
@@ -1,4 +1,4 @@
-import serial,os,time
+import serial,time
 import binascii as bi
 
 FourG_WiFi = "查询失败！"
@@ -12,14 +12,10 @@ class FourGMod(object):
         self.ser.bytesize = 8        #设置数据位
         self.ser.stopbits = 1        #设置停止位
         self.ser.parity = "N"        #设置校验位
-        try:
-            self.ser.open()            #打开串口,要找到对的串口号才会成功
-            if not self.ser.isOpen():
-                with open('error.txt','a') as code:
-                    code.write("4G板串口连接出错！\n")
-        except Exception as e:
+        self.ser.open()            #打开串口,要找到对的串口号才会成功
+        if not self.ser.isOpen():
             with open('error.txt','a') as code:
-                    code.write(str(e) + "4G板串口连接！\n")
+                code.write("4G板串口连接出错！\n")
             
     def port_close(self):
         self.ser.close()
@@ -45,56 +41,19 @@ class FourGMod(object):
         return True
 
     def check_4G_WIFI(self):
-        command = 'AT+CWSSID?\r\r\n'
-        self.ser.read_all()
-        self.send(bi.hexlify(command.encode()).decode())
-        time.sleep(2)
-        feedbacks = self.ser.read_all().decode().split("\n")
-        feedbacks_deal = [fb.strip() for fb in feedbacks if fb.strip() != '']
-        if feedbacks_deal[2].upper() == 'OK':
-            return feedbacks_deal[1][8:]
-        else:
-            return FourG_WiFi
-if __name__ == "__main__":
-    try:
-        os.system("taskkill /f /im AlarmCenterMonitorService.exe")
-        time.sleep(1)
-
-        print("\n【4G板WIFI名称更改设置...名称不能包含中文字符，不能为空】")
-        wifi_model = FourGMod(open('config.txt').read().strip())
         try:
-            print("\n当前WIFI名称：" + wifi_model.check_4G_WIFI())
-        except:
-            wifi_model.setting_4G_WIFI("WaitToUpdateName")
-            time.sleep(7)
-            print("\nWIFI名称包含中文字符，已重置为：WaitToUpdateName")
-
-        wifi_model.control_4G_WIFI('1')
-        input("\nEnter to Continue")
-
-        para = input("\n输入新的WIFI名称：").strip()
-        while(True):
-            if para != '':
-                wifi_model.setting_4G_WIFI(para)
-                time.sleep(7)
-                input("\n名称已更改！点击Enter查阅更改后的名称.")
-
-                try:
-                    print("\n当前WIFI名称：" + wifi_model.check_4G_WIFI())
-                    input("\nEnter TO EXIT!")
-                    break
-                except:
-                    wifi_model.setting_4G_WIFI("WaitToUpdateName")
-                    time.sleep(7)
-                    para = input("\n输入名称异常，已重置为：WaitToUpdateName,请重新输入：")
+            command = 'AT+CWSSID?\r\r\n'
+            self.ser.read_all()
+            self.send(bi.hexlify(command.encode()).decode())
+            time.sleep(2)
+            feedbacks = self.ser.read_all().decode().split("\n")
+            feedbacks_deal = [fb.strip() for fb in feedbacks if fb.strip() != '']
+            self.port_close() 
+            if feedbacks_deal[2].upper() == 'OK':
+                return [self.ser.port,feedbacks_deal[1][8:]]
             else:
-                para = input("\n输入名称为空，请重新输入：")
-            
-        wifi_model.control_4G_WIFI('0')
-        time.sleep(1)
-        while(wifi_model.port_close()):
-            time.sleep(0.5)
-
-    except Exception as e:
-        with open('error.txt','a') as code:
-            code.write(str(e) + "\n main")
+                return [self.ser.port,FourG_WiFi]
+        except Exception as e:
+            with open('error.txt','a') as code:
+                code.write("4G板串口连接出错！" + str(e) + "\n")
+            return [self.ser.port,FourG_WiFi]
