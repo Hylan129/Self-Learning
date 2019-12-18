8c9618a1e151f2c97b2dc391e46f4b7a1af23ff2
Hylan129 <jyzyg129@163.com>
2019-12-18 15:43:55 +0800

    添加4G板的信息

--- robot_application/software_version_check/package/web_produce.py
+++ robot_application/software_version_check/package/web_produce.py
@@ -1,6 +1,6 @@
 from package import file_version,take_picture
 
-def makePage(logo,Mname,Mcode,ip_list,timenow,robot_setting,gaussian_setting,sixmic_info):
+def makePage(logo,Mname,Mcode,ip_list,timenow,robot_setting,gaussian_setting,sixmic_info,FourG_Info):
 
     web_page = open("files/html_base.html",encoding='utf-8').read().replace("\n",'')
     
@@ -8,12 +8,12 @@ def makePage(logo,Mname,Mcode,ip_list,timenow,robot_setting,gaussian_setting,six
 
     if not take_picture.getPicture():
         final_web_page = web_page.format(logo,Mname,Mcode,ip_list,timenow,robot_setting,gaussian_setting,
-            resolution_setting,'','','拍照失败！',sixmic_info)
+            resolution_setting,'','','拍照失败！',sixmic_info,FourG_Info)
     else:
         picture_status = file_version.getFileSizeandTime("files/foreward.jpg")
         final_web_page = web_page.format(logo,Mname,Mcode,ip_list,timenow,robot_setting,gaussian_setting,
             resolution_setting,
             picture_status[0],
-            picture_status[1],'拍照成功！',sixmic_info)
+            picture_status[1],'拍照成功！',sixmic_info,FourG_Info)
 
     return final_web_page
